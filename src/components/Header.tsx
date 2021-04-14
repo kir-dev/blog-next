a24e16695d787b309e9f8aa6f3cd2beb0d1f1913
triszt4n <trisztanpiller@gmail.com>
2021-04-14 21:42:48 +0200

    New navbar on the way

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,3 @@
-import { Box, Flex } from '@chakra-ui/react'
 import * as React from 'react'
 import Navbar from './Navbar'
 
@@ -6,11 +5,6 @@ interface HeaderProps {
   title: string
 }
 
-const Header: React.FC<HeaderProps> = ({ title }) => (
-  <Flex bg="tomato" w="100%" p={4} color="white" justifyContent="space-between">
-    <Box p={4}>{title}</Box>
-    <Navbar />
-  </Flex>
-)
+const Header: React.FC<HeaderProps> = ({ title }) => <Navbar />
 
 export default Header
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,34 +1,239 @@
-import { MoonIcon, SunIcon } from '@chakra-ui/icons'
-import { Box, Flex, IconButton, useColorMode } from '@chakra-ui/react'
-import { Link } from 'gatsby'
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/no-unused-prop-types */
+/* eslint-disable react/require-default-props */
+import { ChevronDownIcon, ChevronRightIcon, CloseIcon, HamburgerIcon, MoonIcon, SunIcon } from '@chakra-ui/icons'
+import {
+  Box,
+  Collapse,
+  Flex,
+  Icon,
+  IconButton,
+  Link,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  Stack,
+  Text,
+  useColorMode,
+  useColorModeValue,
+  useDisclosure
+} from '@chakra-ui/react'
 import * as React from 'react'
 
-const Navbar: React.FC = () => {
+// Source: https://chakra-templates.dev/navigation/navbar
+interface NavItem {
+  label: string
+  children?: Array<NavItem>
+  href: string
+}
+
+const NAV_ITEMS: Array<NavItem> = [
+  {
+    label: 'Kezdőlap',
+    href: '/'
+  },
+  {
+    label: 'Blog',
+    href: '/blog',
+    children: [
+      {
+        label: 'Legújabb posztok',
+        href: '/blog'
+      },
+      {
+        label: 'Archívum',
+        href: '/archive'
+      }
+    ]
+  },
+  {
+    label: 'Rólunk',
+    href: '/about',
+    children: [
+      {
+        label: 'Csapatunk',
+        href: '/about'
+      },
+      {
+        label: 'Projektjeink',
+        href: '/projects'
+      }
+    ]
+  },
+  {
+    label: 'Tanfolyam',
+    href: '/courses'
+  },
+  {
+    label: 'Elérhetőség',
+    href: '/contact'
+  }
+]
+
+const WithSubnavigation: React.FC = () => {
+  const { isOpen, onToggle } = useDisclosure()
   const { colorMode, toggleColorMode } = useColorMode()
+
   return (
-    <Flex bg="tomato" p={4} color="white">
-      <Link to="/">
-        <Box px={4}>Home</Box>
-      </Link>
-      <Link to="/about">
-        <Box px={4}>About us</Box>
-      </Link>
-      <Link to="/blog">
-        <Box px={4}>Blog</Box>
-      </Link>
-      <Link to="/archive">
-        <Box px={4}>Archívum</Box>
-      </Link>
-      <IconButton
-        aria-label="Switch color mode"
-        px={4}
-        h="min"
-        icon={colorMode === 'dark' ? <SunIcon /> : <MoonIcon />}
-        onClick={toggleColorMode}
-        variant="unstyled"
-      />
-    </Flex>
+    <>
+      <Flex
+        bg={useColorModeValue('white', 'gray.800')}
+        color={useColorModeValue('gray.800', 'white')}
+        minH={{ base: '3rem', md: '4.5rem' }}
+        py={{ base: 2 }}
+        px={{ base: 4 }}
+        align="center"
+      >
+        <Flex flex={{ base: 1, md: '1' }} ml={{ base: -2 }} display={{ base: 'flex', md: 'none' }}>
+          <IconButton
+            onClick={onToggle}
+            icon={isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />}
+            variant="ghost"
+            aria-label="Toggle Navigation"
+          />
+        </Flex>
+        <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
+          <Link
+            href="/"
+            display="block"
+            ml="2"
+            rounded="md"
+            _hover={{
+              textDecoration: 'none',
+              color: 'orange.500'
+            }}
+          >
+            <Text textAlign={{ base: 'center', md: 'left' }} fontFamily="heading">
+              Kir-Dev
+            </Text>
+          </Link>
+        </Flex>
+        <Flex display={{ base: 'none', md: 'flex' }} flex={{ base: 1 }} justify={{ base: 'center', md: 'flex-end' }}>
+          <Flex display={{ base: 'none', md: 'flex' }} mx={10}>
+            <DesktopNav />
+          </Flex>
+        </Flex>
+        <Stack flex={{ base: 1, md: 0 }} justify="flex-end" direction="row" spacing={6}>
+          <IconButton
+            aria-label="Sötét-világos mód váltás"
+            px="4"
+            icon={colorMode === 'dark' ? <SunIcon /> : <MoonIcon />}
+            onClick={toggleColorMode}
+            variant="unstyled"
+          />
+        </Stack>
+      </Flex>
+
+      <Collapse in={isOpen} animateOpacity>
+        <MobileNav />
+      </Collapse>
+    </>
   )
 }
 
-export default Navbar
+const DesktopNav: React.FC = () => {
+  return (
+    <Stack direction="row" spacing={4}>
+      {NAV_ITEMS.map(navItem => (
+        <Box key={navItem.label}>
+          <Popover trigger="hover" placement="bottom-start">
+            <PopoverTrigger>
+              <Link
+                p={2}
+                href={navItem.href ?? '#'}
+                fontSize="sm"
+                fontWeight={500}
+                _hover={{
+                  textDecoration: 'none',
+                  color: 'orange.500'
+                }}
+              >
+                {navItem.label}
+              </Link>
+            </PopoverTrigger>
+
+            {navItem.children && (
+              <PopoverContent border={0} boxShadow="xl" bg={useColorModeValue('white', 'gray.800')} p={4} rounded="xl" maxW="2xs">
+                <Stack>
+                  {navItem.children.map(child => (
+                    <DesktopSubNav key={child.label} {...child} />
+                  ))}
+                </Stack>
+              </PopoverContent>
+            )}
+          </Popover>
+        </Box>
+      ))}
+    </Stack>
+  )
+}
+
+const DesktopSubNav: React.FC<NavItem> = ({ label, href }) => {
+  return (
+    <Link href={href} role="group" display="block" p={2} rounded="md" _hover={{ bg: useColorModeValue('orange.50', 'gray.900') }}>
+      <Stack direction="row" align="center">
+        <Box>
+          <Text transition="all .3s ease" _groupHover={{ color: 'orange.500' }} fontWeight={500}>
+            {label}
+          </Text>
+        </Box>
+        <Flex
+          transition="all .3s ease"
+          transform="translateX(-10px)"
+          opacity={0}
+          _groupHover={{ opacity: '100%', transform: 'translateX(0)' }}
+          justify="flex-end"
+          align="center"
+          flex={1}
+        >
+          <Icon color="orange.500" w={5} h={5} as={ChevronRightIcon} />
+        </Flex>
+      </Stack>
+    </Link>
+  )
+}
+
+const MobileNav: React.FC = () => {
+  return (
+    <Stack bg={useColorModeValue('white', 'gray.800')} p={4} display={{ md: 'none' }}>
+      {NAV_ITEMS.map(navItem => (
+        <MobileNavItem key={navItem.label} {...navItem} />
+      ))}
+    </Stack>
+  )
+}
+
+const MobileNavItem: React.FC<NavItem> = ({ label, children, href }) => {
+  const { isOpen, onToggle } = useDisclosure()
+
+  return (
+    <Stack spacing={4} onClick={children && onToggle}>
+      <Flex
+        py={2}
+        as={Link}
+        href={(!children && href) ?? '#'}
+        justify="space-between"
+        align="center"
+        _hover={{
+          textDecoration: 'none'
+        }}
+      >
+        <Text color={useColorModeValue('gray.800', 'gray.200')}>{label}</Text>
+        {children && <Icon as={ChevronDownIcon} transition="all .25s ease-in-out" transform={isOpen ? 'rotate(180deg)' : ''} w={6} h={6} />}
+      </Flex>
+
+      <Collapse in={isOpen} animateOpacity style={{ marginTop: '0' }}>
+        <Stack pl={4} borderLeft={1} borderStyle="solid" borderColor={useColorModeValue('gray.200', 'gray.800')} align="start">
+          {children &&
+            children.map(child => (
+              <Link key={child.label} py={2} href={child.href}>
+                {child.label}
+              </Link>
+            ))}
+        </Stack>
+      </Collapse>
+    </Stack>
+  )
+}
+
+export default WithSubnavigation
