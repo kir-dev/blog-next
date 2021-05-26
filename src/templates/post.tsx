fd1541a492b74d788364eed65c471b1c0fc4ba19
triszt4n <trisztanpiller@gmail.com>
2021-05-26 20:44:16 +0200

    New style for post show page

--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -1,4 +1,4 @@
-import { Flex, Heading, HStack, Text, useColorModeValue } from '@chakra-ui/react'
+import { Box, Flex, Heading, HStack, Text, useColorModeValue } from '@chakra-ui/react'
 import { graphql } from 'gatsby'
 import { GatsbyImage, getImage, ImageDataLike } from 'gatsby-plugin-image'
 import * as React from 'react'
@@ -37,22 +37,44 @@ interface PostTemplateProps {
 
 const PostTemplate: React.FC<PostTemplateProps> = ({ data }) => {
   const result = getImage(data.markdownRemark.frontmatter.featuredImage)
-
+  // const pic =
   return (
     <IndexLayout>
       <Post>
         <Header>
           <Container>
-            {result && <GatsbyImage image={result} alt="Blog preview" objectFit="contain" />}
-            <Heading as="h1" py={5}>
-              {data.markdownRemark.frontmatter.title}
-            </Heading>
-            <Flex direction={{ base: 'column-reverse', sm: 'row' }} justifyContent="space-between">
-              <BlogAuthor longDate name={data.markdownRemark.frontmatter.author} date={new Date(data.markdownRemark.frontmatter.date)} />
-              <HStack fontWeight="light" fontSize="md" px={1} ml="auto" textColor={useColorModeValue('gray.600', 'gray.400')}>
-                <FaClock />
-                <Text>{Math.ceil(data.markdownRemark.fields.readingTime.minutes)} perc</Text>
-              </HStack>
+            <Flex mt={2} direction={{ base: 'column', sm: 'row' }} justifyContent="space-between">
+              <Flex flex={1} position="relative" mr={{ base: 0, sm: 2 }} pb={{ base: 2, sm: 0 }}>
+                <Box w="80%" zIndex={2}>
+                  {result && <GatsbyImage image={result} alt="Blog preview" objectFit="contain" />}
+                </Box>
+                <Box zIndex={1} w="100%" h="100%" position="absolute" ml={1} mt={1}>
+                  <Box bgGradient="radial(orange.400 1px, transparent 1px)" bgSize={{ base: '1.5rem 1.5rem', sm: '1rem 1rem' }} h="100%" />
+                </Box>
+              </Flex>
+              <Flex flex={{ base: 2, md: 4 }} direction="column" justifyContent="center" mt={{ base: 3, sm: 0 }} pl={{ base: 0, sm: 3 }}>
+                <Heading as="h1">{data.markdownRemark.frontmatter.title}</Heading>
+                <Flex justifyContent="space-between" wrap="wrap-reverse">
+                  <BlogAuthor
+                    longDate
+                    name={data.markdownRemark.frontmatter.author}
+                    date={new Date(data.markdownRemark.frontmatter.date)}
+                  />
+                  <HStack
+                    ml={4}
+                    mb={2}
+                    flex={1}
+                    fontWeight="light"
+                    alignItems="center"
+                    justifyContent="flex-end"
+                    fontSize="sm"
+                    textColor={useColorModeValue('gray.600', 'gray.400')}
+                  >
+                    <FaClock />
+                    <Text>{Math.ceil(data.markdownRemark.fields.readingTime.minutes)}&nbsp;perc</Text>
+                  </HStack>
+                </Flex>
+              </Flex>
             </Flex>
           </Container>
         </Header>
