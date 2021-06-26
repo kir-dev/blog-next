5a8ff9c424de39cca39b99d6c19c73427834abc6
Trisztán Piller <60111772+triszt4n@users.noreply.github.com>
2021-06-27 00:36:11 +0200

    Migrate gatsby config files to TS (#92)

---
+++ config/gatsby-config.ts
@@ -0,0 +1,147 @@
+import dotenv from 'dotenv'
+import path from 'path'
+
+dotenv.config({
+  path: `.env.${process.env.NODE_ENV}`
+})
+
+export default {
+  siteMetadata: {
+    baseUrl: 'https://kir-dev.sch.bme.hu',
+    lang: 'hu',
+    title: 'Kir-Dev',
+    titleTemplate: '%s | Kir-Dev',
+    description: 'A Kir-Dev kör blogja',
+    author: 'kir-dev',
+    image: '/favicon.png',
+    keywords: ['gatsbyjs', 'gatsby', 'javascript', 'ruby', 'rails', 'nodejs', 'typescript', 'community', 'simonyi'],
+    robots: 'index, follow'
+  },
+  plugins: [
+    '@chakra-ui/gatsby-plugin',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'post',
+        path: `${path.join(__dirname, '../src/content/posts')}`
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'project',
+        path: `${path.join(__dirname, '../src/content/projects')}`
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'member',
+        path: `${path.join(__dirname, '../src/content/members')}`
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'course',
+        path: `${path.join(__dirname, '../src/content/courses')}`
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${path.join(__dirname, '../src/content/images')}`
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /assets\/images/
+        }
+      }
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-responsive-iframe',
+            options: {
+              wrapperStyle: 'margin-bottom: 1rem'
+            }
+          },
+          {
+            resolve: `gatsby-remark-autolink-headers`,
+            options: {
+              className: `md-headinglink`,
+              isIconAfterHeader: true,
+              elements: [`h1`, `h2`, `h3`, `h4`]
+            }
+          },
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
+          'gatsby-remark-gemoji-to-emoji',
+          'gatsby-remark-external-links',
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 1140,
+              quality: 90,
+              linkImagesToOriginal: false
+            }
+          },
+          {
+            resolve: 'gatsby-remark-vscode',
+            options: {
+              theme: {
+                default: 'Quiet Light',
+                parentSelector: {
+                  'body[class=chakra-ui-dark]': 'Default Dark+'
+                }
+              }
+            }
+          },
+          {
+            resolve: `gatsby-remark-classes`,
+            options: {
+              classMap: {
+                'heading[depth=1]': 'md-h1',
+                'heading[depth=2]': 'md-h2',
+                'heading[depth=3]': 'md-h3',
+                'heading[depth=4]': 'md-h4',
+                'heading[depth=5]': 'md-h5',
+                'heading[depth=6]': 'md-h6',
+                paragraph: 'md-p',
+                'list[ordered=false]': 'md-ul',
+                'list[ordered=true]': 'md-ol',
+                blockquote: 'md-blockquote',
+                listItem: 'md-li',
+                link: 'md-a',
+                tableCell: 'md-td',
+                thematicBreak: 'md-hr',
+                table: 'md-table',
+                inlineCode: 'md-code'
+              }
+            }
+          },
+          `gatsby-remark-reading-time`
+        ]
+      }
+    },
+    'gatsby-transformer-json',
+    {
+      resolve: 'gatsby-plugin-canonical-urls',
+      options: {
+        siteUrl: 'https://kir-dev.sch.bme.hu'
+      }
+    },
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-typescript',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-react-helmet'
+  ]
+}
---
+++ config/gatsby-node.ts
@@ -0,0 +1,81 @@
+import { GatsbyNode, Node } from 'gatsby'
+import { createFilePath } from 'gatsby-source-filesystem'
+import path from 'path'
+
+interface ExtendedNode extends Node {
+  frontmatter: {
+    layout: string
+  }
+}
+
+export const onCreateNode: GatsbyNode['onCreateNode'] = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    const { layout } = (node as ExtendedNode).frontmatter
+    createNodeField({
+      node,
+      name: `slug`,
+      value: `/${layout}${slug}`
+    })
+    createNodeField({
+      node,
+      name: `layout`,
+      value: layout
+    })
+  }
+}
+
+interface EdgesData {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  errors?: any
+  data?: {
+    allMarkdownRemark: {
+      edges: {
+        node: {
+          fields: {
+            layout: string
+            slug: string
+          }
+        }
+      }[]
+    }
+  }
+}
+
+export const createPages: GatsbyNode['createPages'] = async ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  const allMarkdown: EdgesData = await graphql(`
+    {
+      allMarkdownRemark(filter: { fields: { layout: { ne: "course" } } }, limit: 1000) {
+        edges {
+          node {
+            fields {
+              layout
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  if (allMarkdown.errors) {
+    // eslint-disable-next-line no-console
+    console.error(allMarkdown.errors)
+    throw new Error(allMarkdown.errors)
+  }
+
+  allMarkdown.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    const { slug, layout } = node.fields
+
+    createPage({
+      path: slug,
+      component: path.resolve(`./src/templates/${layout}.tsx`),
+      context: {
+        slug
+      }
+    })
+  })
+}
